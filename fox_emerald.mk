84140ef9377bc10232b4683a659a65947677bf27
shezan-tech <ariekechield22@gmail.com>
2025-05-17 21:54:11 +0700

    Update fox_emerald.mk

--- fox_emerald.mk
+++ fox_emerald.mk
@@ -1,1 +1,13 @@
+##########################################
+##### OrangeFox specific build flags #####
+##########################################
 
+# Maintainer
+OF_MAINTAINER := @Androidme77
+
+# Screen
+OF_SCREEN_H := 2400
+OF_STATUS_H := 100
+OF_STATUS_INDENT_LEFT := 48
+OF_STATUS_INDENT_RIGHT := 48
+OF_CLOCK_POS := 1
